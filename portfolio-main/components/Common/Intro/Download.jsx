5785dc4c131913cb14879974296a8d1c68ff3bcf
algrissette <alangrissette02@gmail.com>
2025-02-19 13:37:18 -0500

    trying something

--- portfolio-main/components/Common/Intro/Download.jsx
+++ portfolio-main/components/Common/Intro/Download.jsx
@@ -3,7 +3,7 @@ const Download = ({ icon }) => {
   return (
     <>
       <Link href='/Alan Grissette (4).pdf' target='_blank' className='flex flex-row text-LightGray items-center gap-x-4 pb-14 pt-4'>
-        <span className='text-Snow'>Download Resume</span>
+        <span className='text-Snow'>Download Resuuuuume</span>
         <span>{icon}</span>
       </Link>
     </>
